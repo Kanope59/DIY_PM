972b369bd0e3457f644a780ce785d95e5e80963f
loic <loic@kanope.io>
2021-02-01 15:19:35 +0100

    Commit: Update Doc d'installation, Ajout Doc app Web, Modification du BOM

--- app/arduino_secrets.h
+++ app/arduino_secrets.h
@@ -1,5 +1,5 @@
+#include <Adafruit_HTU21DF.h>
+
 // Replace with keys obtained from TheThingsNetwork console
 #define SECRET_APP_EUI "70B3D57ED002D841";
-#define SECRET_APP_KEY "435F308399CA49D7EB346C852CB64D4B";
-//DEVEUI
-//a8610a3233467d03
+#define SECRET_APP_KEY "C41463D2BBC03C8B70D42D5134004F5D";  
